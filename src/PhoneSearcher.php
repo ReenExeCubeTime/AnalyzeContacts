ddb70977bf25da7ff5e2d54efa44c4d159addee9
ReenExe <ipourri@gmail.com>
2015-11-29 05:06:12 +0200

    1. search short numbers 2. use phpunit for 5.4+ 3. clear from empty contacts

--- src/PhoneSearcher.php
+++ src/PhoneSearcher.php
@@ -39,6 +39,14 @@ class PhoneSearcher extends AbstractSpecialSearcher
         // (0412) 55-15-15
         $list[] = "$code$separator$main";
 
+        // 603
+        // 1560
+        // 555-000
+        $short = range(7, 3);
+        foreach ($short as $length) {
+            $list[] = join($separator, array_fill(0, $length, '\d{1}'));
+        }
+
         $regex = '@(' . join('|', $list). ')@';
 
         if (preg_match_all($regex, $subject, $matches)) {
