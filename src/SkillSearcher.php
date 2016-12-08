c8ba14156f72ca67664f04b8b8b814534eaaeae7
ReenExe <ipourri@gmail.com>
2016-12-08 19:07:14 +0200

    pass tests

--- src/SkillSearcher.php
+++ src/SkillSearcher.php
@@ -4,22 +4,40 @@ namespace ReenExe;
 
 class SkillSearcher extends AbstractSpecialSearcher
 {
-    private $list;
+    private $map;
     private $regex;
 
-    public function __construct($type, array $list)
+    public function __construct($type, array $skills)
     {
         $this->type = $type;
-        $this->list = $list;
-        $this->regex = implode('|', $list);
+
+        $this->map = $this->prepare($skills);
+
+        $this->regex = implode('|', array_keys($this->map));
     }
 
     public function search($subject)
     {
         if (preg_match_all("/\b{$this->regex}\b/i", $subject, $matches)) {
-            return $matches[0];
+            return $this->convert($matches[0]);
         }
 
         return [];
+    }
+
+    private function prepare(array $skills)
+    {
+        $map = [];
+        foreach ($skills as $skill) {
+            $map[strtolower($skill)] = $skill;
+        }
+        return $map;
+    }
+
+    private function convert(array $finds)
+    {
+        $map = array_fill_keys(array_map('strtolower', $finds), true);
+
+        return array_values(array_intersect_key($this->map, $map));
     }
 }
